2341cc633d981fe88038d3b225fc8c85e6d6bc38
Paho Lurie-Gregg <paho@paholg.com>
2014-04-16 02:25:21 -0700

    added make file

---
+++ make.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+import subprocess
+
+subprocess.call(['bibtex', 'inflation'])
+subprocess.call(['pdflatex', 'inflation.tex'])
+#subprocess.call(['pdflatex', 'inflation.tex'])
+#subprocess.call(['pdflatex', 'inflation.tex'])
